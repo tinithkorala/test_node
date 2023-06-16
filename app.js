43cd32a507abda0e3a121b7d934b1fc6af156de3
Tinith K <55810185+tinithkorala@users.noreply.github.com>
2023-06-17 02:17:21 +0530

    Update app.js 111

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 
-
+// Importing required modules
 const express = require('express');
 require('dotenv').config();
 
-// App Configs
+// Creating an Express application
 const app = express();
 
-// Basic Route Handling
+// Setting up the port
+const port = process.env.PORT || 3000;
+
+// Creating a route for the root URL
 app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Basic Route Handling
+app.get('/t', (req, res) => {
     res.status(200).json({message: 'Welcome to evolve API'})
 });
 
-
-// Listen To Server
-const port = process.env.SERVER_PORT || 3000;
+// Starting the server
 app.listen(port, () => {
-    console.log(`Evolve API is running on http://localhost:${port}`);
-})
+  console.log(`Server running at http://localhost:${port}/`);
+});
